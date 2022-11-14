69fb25f6055f68315807f3c50744eefe13feab26
Lucas Silva Souza <lucassilvasouza1@outlook.com>
2022-11-13 23:35:32 -0300

    update services

---
+++ projetomobrj/projetomobrj/CEP.cs
@@ -0,0 +1,31 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetomobrj
+{
+    class MainClass
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepCliente = RestService.For<ICepApiService>("http://viacep.com.br");
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando Informacoes do CEP:" + cepInformado);
+
+                var address = await cepCliente.GetAddressAsync(cepInformado);
+                Console.Write($"\nLogradouro:{address.Logadouro}\nBairro:{address.Bairro}\nCidade:{address.Localidade}\nIbge:{address.Ibge}\nGia:{address.Gia}");
+
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro na consulto de Cep");
+            }
+
+        }
+    }
+}
--- projetomobrj/projetomobrj/MainPage.xaml.cs
+++ projetomobrj/projetomobrj/MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Refit;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace projetomobrj
@@ -16,7 +18,17 @@ namespace projetomobrj
         }
         private async void Button_Clicked(object sender, EventArgs e)
         {
+            var cepCliente = RestService.For<ICepApiService>("http://viacep.com.br");
+            //string cepInformado = Console.ReadLine().ToString();
+            string cepInformado = "79104330";
+            Console.WriteLine("Consultando Informacoes do CEP:" + cepInformado);
+
+            var address = await cepCliente.GetAddressAsync(cepInformado);
+            Preferences.Set("logradouro", address.Logadouro);
+            Console.Write($"\nLogradouro:{address.Logadouro}\nBairro:{address.Bairro}\nCidade:{address.Localidade}\nIbge:{address.Ibge}\nGia:{address.Gia}");
+
             await Navigation.PushAsync(new mapa());
-        }
+        }    
+    
     }
 }
